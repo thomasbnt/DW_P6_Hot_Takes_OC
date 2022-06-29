7c677e3db9c404a0a5c371f213deb79d13a83cbb
Thomas Bnt <thomasbnt@protonmail.com>
2022-06-29 21:44:47 +0200

    Init Express + exemple route /devto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,53 @@
 const express = require('express');
-
 const app = express();
+app.disable("x-powered-by");
 
-app.listen(3000, () =>
-    console.log('App listening on port 3000!'),
-);
+const fetch = require("node-fetch");
+
+
+/*
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+*/
+
+// Toutes les routes
+app.use((req, res, next) => {
+    console.log('Request received');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/auth/signup', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    console.log('Signup request received');
+    next();
+});
+
+// Pour les tests de JSON avec l'API de dev.to
+app.use('/devto', (req, res, next) => {
+    // Récupère depuis https://dev.to/api/articles/latest?username=thomasbnt
+    fetch('https://dev.to/api/articles/latest?username=thomasbnt')
+        .then(response => response.json())
+        .then(data => {
+            let allArticles = [];
+            data.forEach(article => {
+                allArticles.push({
+                    title: article.title,
+                    url: article.url,
+                    description: article.description,
+                    published_at: article.published_at,
+                    tags: article.tag_list
+                });
+            })
+            res.json(allArticles);
+            next();
+        });
+})
+
+/*app.use((req, res, next) => {
+    res.json({message: 'Hello World !'});
+})*/
+
+module.exports = app;
