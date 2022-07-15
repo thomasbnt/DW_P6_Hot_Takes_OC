90130d97b836c4c5b33c3fd706f50b1173917635
Thomas Bnt <thomasbnt@protonmail.com>
2022-07-15 16:16:06 +0200

    supp mongoose-unique-validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,8 @@
 const user = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const signupSchema = new user.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 });
-
-signupSchema.plugin(uniqueValidator);
 
 module.exports = user.model('User', signupSchema);
