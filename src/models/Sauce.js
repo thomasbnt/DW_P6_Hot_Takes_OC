2b96150e73cccc665422d781d60371d964808ea9
Thomas Bnt <thomasbnt@protonmail.com>
2022-07-25 20:33:23 +0200

    removed unique:true to name on schema model

--- src/models/Sauce.js
+++ src/models/Sauce.js
@@ -1,12 +1,13 @@
 const user = require('mongoose');
 
 const sauceSchema = new user.Schema({
-    name: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
     manufacturer: {type: String, required: true},
     description: {type: String, required: true},
     mainPepper: {type: String, required: true},
     imageUrl: {type: String, required: true},
     heat: {type: Number, required: true},
+
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
     usersLiked: {type: [String]},
