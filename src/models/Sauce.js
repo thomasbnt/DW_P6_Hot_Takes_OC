54b2b8fa9931457d825efd985bacfae5fdeaebb4
Thomas Bnt <thomasbnt@protonmail.com>
2022-07-26 15:43:20 +0200

    usersLiked/usersDisliked : default:t rue

--- src/models/Sauce.js
+++ src/models/Sauce.js
@@ -10,8 +10,8 @@ const sauceSchema = new user.Schema({
 
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
-    usersLiked: {type: [String]},
-    usersDisliked: {type: [String]},
+    usersLiked: {type: [String], default : []},
+    usersDisliked: {type: [String], default : []},
 });
 
 module.exports = user.model('Sauce', sauceSchema);
