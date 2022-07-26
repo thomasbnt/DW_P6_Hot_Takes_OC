cbe6ae586ad5bb8647e8dbd232b7801cab8df6b8
Thomas Bnt <thomasbnt@protonmail.com>
2022-07-26 15:43:32 +0200

    update route SaucesCtrl

--- src/routes/sauces.js
+++ src/routes/sauces.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const cors = require("cors");
 const SaucesCtrl = require('../controllers/sauces');
 
 const auth = require('../middlewares/authenticateToken');
 const upload = require('../middlewares/multer');
 
-router.get('/', auth, SaucesCtrl.GetAllSauces);
-router.get('/:id', auth, SaucesCtrl.GetSaucesPerID);
-router.post('/', auth, upload.single('image'), SaucesCtrl.CreateSauce);
-router.put('/:id', auth, SaucesCtrl.UpdateSauce);
-router.delete('/:id', auth, SaucesCtrl.DeleteSauce);
-router.post('/:id/like', auth, SaucesCtrl.LikeSauce);
+
+router.get('/', cors({methods: "GET"}), auth, SaucesCtrl.GetAllSauces);
+router.get('/:id', cors({methods: "GET"}), auth, SaucesCtrl.GetSaucesPerID);
+router.post('/', cors({methods: "POST"}), auth, upload.single('image'), SaucesCtrl.CreateSauce);
+router.put('/:id', cors({methods: "PUT"}), auth, SaucesCtrl.UpdateSauce);
+router.delete('/:id', cors({methods: "DELETE"}), auth, SaucesCtrl.DeleteSauce);
+router.post('/:id/like', cors({methods: "POST"}), auth, SaucesCtrl.LikeSauce);
 
 module.exports = router;
