534b826dc900d95c6dcb6127fb890456042ab39a
Thomas Bnt <thomasbnt@protonmail.com>
2022-08-01 22:18:15 +0200

    Added getFileNameWithoutExtension multer

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,14 +4,20 @@ const MIME_TYPE_MAP = {
     "image/jpeg": "jpg",
     "image/jpg": "jpg"
 }
+
+function getFileNameWithoutExtension(filename) {
+    return filename.split('.').slice(0, -1).join('.')
+}
+
+
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'src/images')
     },
     filename: (req, file, cb) => {
-        /*console.log(file)
-        console.log(MIME_TYPE_MAP[file.mimetype])*/
-        cb(null, `${file.originalname}-${+Date.now()}.${MIME_TYPE_MAP[file.mimetype]}`)
+        /*console.log(MIME_TYPE_MAP[file.mimetype])*/
+        cb(null, `${getFileNameWithoutExtension(file.originalname)}-${+Date.now()}.${MIME_TYPE_MAP[file.mimetype]}`)
     }
 })
 
